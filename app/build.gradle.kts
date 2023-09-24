9679ec90f66d863ec4cd39eea79254b6464e1054
PrimoDev23 <lexx.ps2711@gmail.com>
2023-09-24 10:23:44 +0200

    Add dagger hilt gradle plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.com.android.application)
     alias(libs.plugins.org.jetbrains.kotlin.android)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.dagger)
 }
 
 android {
