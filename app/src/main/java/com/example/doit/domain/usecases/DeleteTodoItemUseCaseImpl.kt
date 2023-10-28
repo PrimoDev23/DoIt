85a609813417a9d7c4f4a8f8084d0fad1ac9962e
PrimoDev23 <lexx.ps2711@gmail.com>
2023-10-28 21:57:22 +0200

    Simplify deleting todo items * The foreign key is used for deleting anyways

--- app/src/main/java/com/example/doit/domain/usecases/DeleteTodoItemUseCaseImpl.kt
+++ app/src/main/java/com/example/doit/domain/usecases/DeleteTodoItemUseCaseImpl.kt
@@ -3,7 +3,6 @@ package com.example.doit.domain.usecases
 import android.content.Context
 import androidx.work.WorkManager
 import com.example.doit.domain.models.TodoItem
-import com.example.doit.domain.repositories.SubtaskRepository
 import com.example.doit.domain.repositories.TodoItemRepository
 import com.example.doit.domain.usecases.interfaces.DeleteTodoItemsUseCase
 import dagger.hilt.android.qualifiers.ApplicationContext
@@ -11,8 +10,7 @@ import javax.inject.Inject
 
 class DeleteTodoItemUseCaseImpl @Inject constructor(
     @ApplicationContext private val context: Context,
-    private val todoItemRepository: TodoItemRepository,
-    private val subtaskRepository: SubtaskRepository
+    private val todoItemRepository: TodoItemRepository
 ) : DeleteTodoItemsUseCase {
     override suspend fun delete(items: List<TodoItem>) {
         val workManager = WorkManager.getInstance(context)
@@ -20,8 +18,6 @@ class DeleteTodoItemUseCaseImpl @Inject constructor(
         items.forEach {
             todoItemRepository.deleteItemById(it.id)
             workManager.cancelAllWorkByTag(it.id)
-
-            subtaskRepository.deleteByParent(it.id)
         }
     }
 }
