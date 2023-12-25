83198a987ad1c79707ece82f441ccf16b64375aa
PrimoDev23 <lexx.ps2711@gmail.com>
2023-12-25 20:24:53 +0100

    Add test for DeleteTagsUseCase

---
+++ app/src/test/java/com/example/doit/domain/usecases/DeleteTagsUseCaseTest.kt
@@ -0,0 +1,52 @@
+package com.example.doit.domain.usecases
+
+import com.example.doit.TestBase
+import com.example.doit.data.Tags
+import com.example.doit.data.TodoItems
+import com.example.doit.domain.repositories.TagRepository
+import com.example.doit.domain.repositories.TodoItemRepository
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+
+class DeleteTagsUseCaseTest : TestBase() {
+
+    private val todoListTagOne = listOf(
+        TodoItems.todoItemOne,
+        TodoItems.todoItemThree
+    )
+
+    private val todoListTagOneWithout = listOf(
+        TodoItems.todoItemOne.copy(tags = TodoItems.todoItemOne.tags - Tags.tagOne),
+        TodoItems.todoItemThree.copy(tags = TodoItems.todoItemThree.tags - Tags.tagOne)
+    )
+
+    @Test
+    fun invoke() = runTest {
+        val tagRepo = mockk<TagRepository>()
+        val todoRepo = mockk<TodoItemRepository>()
+
+        val tags = listOf(Tags.tagOne)
+        val ids = tags.map { it.id }
+
+        coEvery { todoRepo.getItemsWithTagIds(ids) } returns todoListTagOne
+        coEvery { todoRepo.saveTodoItems(any()) } returns Unit
+        coEvery { tagRepo.deleteTags(any()) } returns Unit
+
+        val useCase = DeleteTagsUseCaseImpl(
+            tagRepository = tagRepo,
+            todoItemRepository = todoRepo
+        )
+
+        useCase.invoke(tags)
+
+        coVerify { todoRepo.getItemsWithTagIds(ids) }
+
+        coVerify { todoRepo.saveTodoItems(todoListTagOneWithout) }
+
+        coVerify { tagRepo.deleteTags(tags) }
+    }
+
+}
