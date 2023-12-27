be7545f31f024da333fe592e4b4115087e12d912
PrimoDev23 <lexx.ps2711@gmail.com>
2023-12-27 01:47:48 +0100

    Add tests for SubtaskRepository

---
+++ app/src/test/java/com/example/doit/data/repositories/SubtaskRepositoryTest.kt
@@ -0,0 +1,100 @@
+package com.example.doit.data.repositories
+
+import com.example.doit.TestBase
+import com.example.doit.data.Subtasks
+import com.example.doit.data.daos.SubtaskDao
+import com.example.doit.data.models.local.SubtaskEntity
+import com.example.doit.data.models.local.toEntity
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class SubtaskRepositoryTest : TestBase() {
+
+    @Test
+    fun getSubtasksByParent() = runTest {
+        val dao = mockk<SubtaskDao>()
+
+        val parent = "12345"
+        val subtasks = Subtasks.subtasks
+        val entities = subtasks.map {
+            it.toEntity(parent)
+        }
+
+        coEvery { dao.selectByParent(parent) } returns entities
+
+        val repo = SubtaskRepositoryImpl(
+            subtaskDao = dao
+        )
+
+        val result = repo.getSubtasksByParent(parent)
+
+        coVerify { dao.selectByParent(parent) }
+
+        assertEquals(subtasks, result)
+    }
+
+    @Test
+    fun saveSubtasksForParent() = runTest {
+        val dao = mockk<SubtaskDao>()
+
+        val parent = "12345"
+
+        coEvery { dao.insertOnSave(parent, any()) } returns Unit
+
+        val repo = SubtaskRepositoryImpl(
+            subtaskDao = dao
+        )
+
+        val subtasks = Subtasks.subtasks
+
+        repo.saveSubtasksForParent(parent, subtasks)
+
+        val entities = subtasks.map {
+            it.toEntity(parent)
+        }
+
+        coVerify { dao.insertOnSave(parent, entities) }
+    }
+
+    @Test
+    fun saveSubtaskForParent() = runTest {
+        val dao = mockk<SubtaskDao>()
+
+        val parent = "12345"
+
+        coEvery { dao.insert(any<SubtaskEntity>()) } returns Unit
+
+        val repo = SubtaskRepositoryImpl(
+            subtaskDao = dao
+        )
+
+        val subtask = Subtasks.subtaskOne
+
+        repo.saveSubtaskForParent(parent, subtask)
+
+        val entity = subtask.toEntity(parent)
+
+        coVerify { dao.insert(entity) }
+    }
+
+    @Test
+    fun deleteByParent() = runTest {
+        val dao = mockk<SubtaskDao>()
+
+        val parent = "12345"
+
+        coEvery { dao.deleteByParent(any()) } returns Unit
+
+        val repo = SubtaskRepositoryImpl(
+            subtaskDao = dao
+        )
+
+        repo.deleteByParent(parent)
+
+        coVerify { dao.deleteByParent(parent) }
+    }
+}
