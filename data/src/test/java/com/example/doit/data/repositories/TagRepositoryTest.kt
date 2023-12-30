b9e8a2d6069b1c9b8fad7ff18fdbb7d9589b1bd1
PrimoDev23 <lexx.ps2711@gmail.com>
2023-12-30 10:58:23 +0100

    Test getTagsForItem

--- data/src/test/java/com/example/doit/data/repositories/TagRepositoryTest.kt
+++ data/src/test/java/com/example/doit/data/repositories/TagRepositoryTest.kt
@@ -1,9 +1,13 @@
 package com.example.doit.data.repositories
 
 import com.example.doit.data.daos.TagDao
+import com.example.doit.data.models.TodoItemWithSubtasksEntity
+import com.example.doit.data.models.getTagsForItem
 import com.example.doit.data.models.toEntity
+import com.example.doit.domain.models.Tag
 import com.example.doit.testing.Tags
 import com.example.doit.testing.TestBase
+import com.example.doit.testing.TodoItems
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
@@ -126,5 +130,48 @@ class TagRepositoryTest : TestBase() {
                 dao.delete(it)
             }
         }
+    }
+
+    @Test
+    fun getTagsForItem() = runTest {
+        val dao = mockk<TagDao>()
+
+        val item = TodoItems.todoItemOne
+        val tags = item.tags
+        val tagIds = tags.map {
+            it.id
+        }
+        val entities = tags.map {
+            it.toEntity()
+        }
+
+        val fullItemEntityNoTags = TodoItemWithSubtasksEntity(
+            item = item.copy(tags = emptyList()).toEntity(),
+            subtasks = item.subtasks.map {
+                it.toEntity(item.id)
+            }
+        )
+        val fullItemEntity = TodoItemWithSubtasksEntity(
+            item = item.toEntity(),
+            subtasks = item.subtasks.map {
+                it.toEntity(item.id)
+            }
+        )
+
+        coEvery { dao.selectByIds(tagIds) } returns entities
+
+        val repo = TagRepositoryImpl(
+            dao = dao
+        )
+
+        val noTagResult = repo.getTagsForItem(fullItemEntityNoTags)
+
+        assertEquals(emptyList<Tag>(), noTagResult)
+
+        val tagResult = repo.getTagsForItem(fullItemEntity)
+
+        coVerify { dao.selectByIds(tagIds) }
+
+        assertEquals(tags, tagResult)
     }
 }
